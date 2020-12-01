7895aafa8da67506a26a82851530bbeb25e27f6e
wilsonericn <wilson.eric.n@gmail.com>
2020-12-01 12:05:51 -0500

    cleanup

--- aoc/day01.py
+++ aoc/day01.py
@@ -3,21 +3,21 @@ from aoc.util import puzzle_input
 
 def compute1():
     input_nums = puzzle_input(__file__, int)
-    for n1 in range(len(input_nums)):
-        for n2 in range(n1, len(input_nums)):
-            if input_nums[n1] + input_nums[n2] == 2020:
-                print(input_nums[n1], input_nums[n2])
-                print(input_nums[n1] * input_nums[n2])
+    for i1, n1 in enumerate(input_nums):
+        for i2, n2 in enumerate(input_nums):
+            if i1 < i2 and n1 + n2 == 2020:
+                print(n1, n2)
+                print(n1 * n2)
 
 
 def compute2():
     input_nums = puzzle_input(__file__, int)
-    for n1 in range(len(input_nums)):
-        for n2 in range(n1, len(input_nums)):
-            for n3 in range(n2, len(input_nums)):
-                if input_nums[n1] + input_nums[n2] + input_nums[n3] == 2020:
-                    print(input_nums[n1], input_nums[n2], input_nums[n3])
-                    print(input_nums[n1] * input_nums[n2] * input_nums[n3])
+    for i1, n1 in enumerate(input_nums):
+        for i2, n2 in enumerate(input_nums):
+            for i3, n3 in enumerate(input_nums):
+                if i1 < i2 < i3 and n1 + n2 + n3 == 2020:
+                    print(n1, n2, n3)
+                    print(n1 * n2 * n3)
 
 
 if __name__ == '__main__':
