3c2d852e474994bda5ee97833dffcccd415b8f89
wilsonericn <wilson.eric.n@gmail.com>
2020-12-01 14:44:06 -0500

    cleanup

--- aoc/day01.py
+++ aoc/day01.py
@@ -3,21 +3,14 @@ from aoc.util import puzzle_input
 
 def compute1():
     input_nums = puzzle_input(__file__, int)
-    for i1, n1 in enumerate(input_nums):
-        for i2, n2 in enumerate(input_nums):
-            if i1 < i2 and n1 + n2 == 2020:
-                print(n1, n2)
-                print(n1 * n2)
+    pair = next(((n1, n2) for i1, n1 in enumerate(input_nums) for i2, n2 in enumerate(input_nums) if i1 < i2 and n1 + n2 == 2020))
+    print(pair, pair[0] * pair[1])
 
 
 def compute2():
     input_nums = puzzle_input(__file__, int)
-    for i1, n1 in enumerate(input_nums):
-        for i2, n2 in enumerate(input_nums):
-            for i3, n3 in enumerate(input_nums):
-                if i1 < i2 < i3 and n1 + n2 + n3 == 2020:
-                    print(n1, n2, n3)
-                    print(n1 * n2 * n3)
+    triple = next(((n1, n2, n3) for i1, n1 in enumerate(input_nums) for i2, n2 in enumerate(input_nums) for i3, n3 in enumerate(input_nums) if i1 < i2 < i3 and n1 + n2 + n3 == 2020))
+    print(triple, triple[0] * triple[1] * triple[2])
 
 
 if __name__ == '__main__':
