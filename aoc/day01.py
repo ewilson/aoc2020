690e008c8c532f242250525fdd08d3ae1d1a5653
wilsonericn <wilson.eric.n@gmail.com>
2020-12-01 15:51:57 -0500

    final cleanup, prep for day 2

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,18 +1,17 @@
 from aoc.util import puzzle_input
 
 
-def compute1():
-    input_nums = puzzle_input(__file__, int)
-    pair = next(((n1, n2) for i1, n1 in enumerate(input_nums) for i2, n2 in enumerate(input_nums) if i1 < i2 and n1 + n2 == 2020))
+def compute1(nums):
+    pair = next(((n1, n2) for i1, n1 in enumerate(nums) for i2, n2 in enumerate(nums) if i1 < i2 and n1 + n2 == 2020))
     print(pair, pair[0] * pair[1])
 
 
-def compute2():
-    input_nums = puzzle_input(__file__, int)
-    triple = next(((n1, n2, n3) for i1, n1 in enumerate(input_nums) for i2, n2 in enumerate(input_nums) for i3, n3 in enumerate(input_nums) if i1 < i2 < i3 and n1 + n2 + n3 == 2020))
+def compute2(nums):
+    triple = next(((n1, n2, n3) for i1, n1 in enumerate(nums) for i2, n2 in enumerate(nums) for i3, n3 in enumerate(nums) if i1 < i2 < i3 and n1 + n2 + n3 == 2020))
     print(triple, triple[0] * triple[1] * triple[2])
 
 
 if __name__ == '__main__':
-    compute1()
-    compute2()
+    data = puzzle_input(__file__, int)
+    compute1(data)
+    compute2(data)
---
+++ aoc/day02.py
@@ -0,0 +1,15 @@
+from aoc.util import puzzle_input
+
+
+def compute1(nums):
+    pass
+
+
+def compute2(nums):
+    pass
+
+
+if __name__ == '__main__':
+    data = puzzle_input(__file__)
+    compute1(data)
+    compute2(data)
