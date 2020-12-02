36412899d61d6772c759db2746630562582f4e2f
wilsonericn <wilson.eric.n@gmail.com>
2020-12-02 06:06:04 -0500

    works, pretty ugly

--- aoc/day02.py
+++ aoc/day02.py
@@ -1,15 +1,39 @@
-from aoc.util import puzzle_input
+from collections import Counter
+
+from aoc.util import multiline_input
 
 
-def compute1(nums):
-    pass
+def compute1(data):
+    return len([d for d in data if validate(d[0], d[1])])
+
+
+def validate(policy, password):
+    minmax, char = policy.split(' ')
+    minmaxl = minmax.split('-')
+    minc = int(minmaxl[0])
+    maxc = int(minmaxl[1])
+    pwc = Counter(password)
+    return minc <= pwc[char] <= maxc
+
+
+def validate2(policy, password):
+    onetwo, char = policy.split(' ')
+    onetwol = onetwo.split('-')
+    one = int(onetwol[0])
+    two = int(onetwol[1])
+    print(policy, password, password[one-1] == char and password[two-1] != char)
+    print(f'character {one} is {char} ({password[one-1]}), character {two} ({password[two-1]}) is twot')
+    return (password[one-1] == char and password[two-1] != char) or (password[one-1] != char and password[two-1] == char)
 
 
 def compute2(nums):
-    pass
+    return len([d for d in data if validate2(d[0], d[1])])
 
 
+# 3-8 v: knvrrqvtv
 if __name__ == '__main__':
-    data = puzzle_input(__file__)
+    data = multiline_input(__file__, lambda line: line.split(': '))
     result1 = compute1(data)
+    print(result1)
     result2 = compute2(data)
+    print(result2)
