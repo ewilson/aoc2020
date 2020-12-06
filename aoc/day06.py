fcda019ac3dfca8736780baf549da6c0688d5cec
wilsonericn <wilson.eric.n@gmail.com>
2020-12-06 12:28:59 -0500

    day 6

---
+++ aoc/day06.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+from aoc.util import multiple_line_records, verify
+
+
+def answers(record):
+    c = Counter([r for r in record if r != ' '])
+    return c.keys()
+
+
+def answers2(record):
+    return set.intersection(*[set(a) for a in record.split()])
+
+
+def compute1(records, expected=None):
+    total = sum([len(answers(r)) for r in records])
+    return verify(expected, total)
+
+
+def compute2(records, expected=None):
+    total = sum([len(answers2(r)) for r in records])
+    return verify(expected, total)
+
+
+if __name__ == '__main__':
+    test_data = multiple_line_records(__file__, test=True)
+    data = multiple_line_records(__file__)
+    compute1(test_data, 11)
+    result1 = compute1(data, 6680)
+    print(result1)
+    compute2(test_data, 6)
+    result2 = compute2(data, 3117)
+    print(result2)
