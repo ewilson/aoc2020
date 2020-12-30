58a2aac425bccbe8f43aee4a5bbad7531fe424c1
wilsonericn <wilson.eric.n@gmail.com>
2020-12-29 22:00:41 -0500

    day 22 refactor

--- aoc/day22.py
+++ aoc/day22.py
@@ -29,32 +29,30 @@ def can_recurse(val, hand):
     return val <= len(hand)
 
 
+def find_winner(card1, hand1, card2, hand2):
+    if can_recurse(card1, hand1) and can_recurse(card2, hand2):
+        winner1, _, __ = play(hand1[:card1], hand2[:card2])
+        return winner1
+    else:
+        return card1 > card2
+
+
 def play(hand1, hand2):
-    print(hand1, hand2)
-    hand1, hand2 = list(hand1), list(hand2)
-    states = []
+    states = set()
     while hand1 and hand2:
-        if (hand1, hand2) in states:
+        if (tuple(hand1), tuple(hand2)) in states:
             return True, score(hand1), score(hand2)
         else:
-            states.append((list(hand1), list(hand2)))
+            states.add((tuple(hand1), tuple(hand2)))
         card1 = hand1.pop(0)
         card2 = hand2.pop(0)
-        if can_recurse(card1, hand1) and can_recurse(card2, hand2):
-            winner1, _, __ = play(hand1[:card1], hand2[:card2])
-            if winner1:
-                hand1.append(card1)
-                hand1.append(card2)
-            else:
-                hand2.append(card2)
-                hand2.append(card1)
+        winner1 = find_winner(card1, hand1, card2, hand2)
+        if winner1:
+            hand1.append(card1)
+            hand1.append(card2)
         else:
-            if card1 > card2:
-                hand1.append(card1)
-                hand1.append(card2)
-            else:
-                hand2.append(card2)
-                hand2.append(card1)
+            hand2.append(card2)
+            hand2.append(card1)
 
     if hand1:
         return True, score(hand1), score(hand2)
@@ -75,4 +73,4 @@ if __name__ == '__main__':
     test_solution(compute1, test_data, 306)
     test_solution(compute1, data, 31314)
     test_solution(compute2, test_data, 291)
-    test_solution(compute2, data)
+    test_solution(compute2, data, 32760)
